4359c8c09225db2359b1695ab0b248883c3895ad
Olli <91615532+linndfors@users.noreply.github.com>
2023-05-22 17:01:24 +0200

    Update RecommendedDish.js

--- src/components/RecommendedDish.js
+++ src/components/RecommendedDish.js
@@ -3,27 +3,23 @@ import './RecommendedDish.css';
 
 const RecommendedDish = (props) => {
   return (
-<div className = "dish-block">
-    <img src={props.imgurl} className = "recommendation-photo" alt="User account"/>
-    <div className="overlay">
-      <pre className="text">
-      Time to cook: {props.time} min<br></br>
-      Type: {props.dishtype}<br></br>
-      Main Ingredients:<br></br>
-      {props.ingredients1}<br></br>
-      {props.ingredients2}<br></br>
-      {props.ingredients3}<br></br>
-      {props.ingredients4}<br></br>
-      {props.ingredients5}<br></br>
-      </pre>
-      {/* <p className="text">Ingredients: {props.data.extendedIngredients[0].name}</p>
-      <p className="text">Ingredients: {props.data.extendedIngredients[1].name}</p> */}
-
+    <div className="dish-block">
+      <img src={props.imgurl} className="recommendation-photo" alt="User account" />
+      <div className="overlay">
+        <pre className="text">
+          Time to cook: {props.time} min<br />
+          Type: {props.dishtype}<br />
+          Main Ingredients:<br />
+          {props.ingredients1}<br />
+          {props.ingredients2}<br />
+          {props.ingredients3}<br />
+          {props.ingredients4}<br />
+          {props.ingredients5}<br />
+        </pre>
+      </div>
+      <h3>{props.title}</h3>
     </div>
-    {/* <div className="recommendation-photo recommendation-photo-1"></div> */}
-    <h3>{props.title}</h3>
-</div>
   );
 };
 
-export default RecommendedDish;
+export default React.memo(RecommendedDish);
